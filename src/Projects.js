d5aefa6e4bac941723948335f1ffcee342fb3af6
taiwo2 <adetonat@ymail.com>
2021-04-12 08:44:36 +0100

    Fix Linters

--- src/Projects.js
+++ src/Projects.js
@@ -1,65 +1,63 @@
-import { LocalDB } from './LocalDB';
-import { Project } from './Project';
+import { LocalDB } from './LocalDB'
+import { Project } from './Project'
 
 const Projects = (() => {
+  const projects = LocalDB.getProjects()
 
-    let projects = LocalDB.getProjects();
+  const addProject = (title, description) => {
+    projects.push(Project(title, description))
+    LocalDB.saveProjects(projects)
+  }
 
-    const addProject = (title, description) => {
-        projects.push(Project(title, description));
-        LocalDB.saveProjects(projects);
-    }
+  const editProject = (index, title, description) => {
+    LocalDB.removeToDos(projects[index].title)
+    projects[index].title = title
+    projects[index].description = description
+    LocalDB.saveProjects(projects)
+    LocalDB.saveTodo(projects[index].title, projects[index].getAllToDos())
+  }
 
-    const editProject = (index, title, description) => {
-        LocalDB.removeToDos(projects[index].title);
-        projects[index].title = title;
-        projects[index].description = description;
-        LocalDB.saveProjects(projects);
-        LocalDB.saveTodo(projects[index].title, projects[index].getAllToDos());
-    }
+  const getAllProjects = () => {
+    return projects
+  }
 
-    const getAllProjects = () => {
-        return projects;
-    }
+  const addToDoOnProject = (index, title) => {
+    const today = new Date()
+    const day = today.getDate()
+    const month = today.getMonth()
+    const year = today.getFullYear()
+    const hours = today.getHours()
+    const minutes = today.getMinutes()
+    projects[index].addToDo(title, '', new Date(year, month, day, hours, minutes), 'Low', '', false)
+    LocalDB.saveTodo(projects[index].title, projects[index].getAllToDos())
+  }
 
-    const addToDoOnProject = (index, title) => {
-        let today = new Date();
-        let day = today.getDate();
-        let month = today.getMonth();
-        let year = today.getFullYear();
-        let hours = today.getHours();
-        let minutes = today.getMinutes();
-        projects[index].addToDo(title, "", new Date(year,month,day,hours,minutes), "Low", "", false);
-        LocalDB.saveTodo(projects[index].title, projects[index].getAllToDos());
+  const getProject = (index) => {
+    return projects[index]
+  }
 
-    }
+  const removeProject = (index) => {
+    LocalDB.removeToDos(projects[index].title)
+    projects.splice(index, 1)
+    LocalDB.saveProjects(projects)
+  }
 
-    const getProject = (index) => {
-        return projects[index];
-    }
+  const deleteToDofromProject = (indexProject, indexToDo) => {
+    projects[indexProject].removeToDo(indexToDo)
+    LocalDB.saveTodo(projects[indexProject].title, projects[indexProject].getAllToDos())
+  }
 
-    const removeProject = (index) => {
-        LocalDB.removeToDos(projects[index].title);
-        projects.splice(index, 1);
-        LocalDB.saveProjects(projects);
-    }
+  const editToDofromProject = (indexProject, indexTodo, title, description, duedate, notes, priority) => {
+    projects[indexProject].editToDo(indexTodo, title, description, duedate, notes, priority)
+    LocalDB.saveTodo(projects[indexProject].title, projects[indexProject].getAllToDos())
+  }
 
-    const deleteToDofromProject = (indexProject, indexToDo) => {
-        projects[indexProject].removeToDo(indexToDo);
-        LocalDB.saveTodo(projects[indexProject].title, projects[indexProject].getAllToDos());
-    }
+  const changeCheck = (indexProject, indexToDo, checked) => {
+    projects[indexProject].changeCheckedToDo(indexToDo, checked)
+    LocalDB.saveTodo(projects[indexProject].title, projects[indexProject].getAllToDos())
+  }
 
-    const editToDofromProject = (indexProject, indexTodo, title, description, duedate, notes, priority)=>{
-        projects[indexProject].editToDo(indexTodo, title, description, duedate, notes, priority);
-        LocalDB.saveTodo(projects[indexProject].title, projects[indexProject].getAllToDos());
-    }
+  return { addProject, getAllProjects, removeProject, editProject, getProject, addToDoOnProject, deleteToDofromProject, editToDofromProject, changeCheck }
+})()
 
-    const changeCheck = (indexProject,indexToDo, checked)=>{
-        projects[indexProject].changeCheckedToDo(indexToDo, checked);
-        LocalDB.saveTodo(projects[indexProject].title, projects[indexProject].getAllToDos());
-    }
-
-    return { addProject, getAllProjects, removeProject, editProject, getProject, addToDoOnProject, deleteToDofromProject, editToDofromProject, changeCheck };
-})();
-
-export {Projects};
+export { Projects }
--- src/ToDo.js
+++ src/ToDo.js
@@ -1,26 +1,25 @@
 const ToDo = (title, description, duedate, priority, notes, checked) => {
-    if(!(duedate instanceof Date)){
-        duedate = new Date(duedate);
-    }
+  if (!(duedate instanceof Date)) {
+    duedate = new Date(duedate)
+  }
 
-    const getDueDate = () => {
-        return duedate;
-    }
+  const getDueDate = () => {
+    return duedate
+  }
 
-    const setDueDate = (newDate) => {
-        duedate = newDate; 
-    }
+  const setDueDate = (newDate) => {
+    duedate = newDate
+  }
 
-    const getChecked = () => {
-        return checked;
-    }
+  const getChecked = () => {
+    return checked
+  }
 
-    const changeCheck = () => {
-        checked = !checked;
-    }
+  const changeCheck = () => {
+    checked = !checked
+  }
 
-    return { title, description, priority, notes, duedate, checked, getDueDate, setDueDate, getChecked, changeCheck };
-
+  return { title, description, priority, notes, duedate, checked, getDueDate, setDueDate, getChecked, changeCheck }
 }
 
-export { ToDo };
+export { ToDo }
