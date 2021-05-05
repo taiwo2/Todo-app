4c7fef15ba2f1729a3182441dd31a60084f4b1f6
taiwo2 <adetonat@ymail.com>
2021-05-05 16:02:08 +0100

    fix linters errors

--- test/Project.test.js
+++ test/Project.test.js
@@ -1,4 +1,4 @@
-import { ToDo, Project, getAllToDos } from '../src/Project';
+import { Project } from '../src/Project';
 
 const kProject = Project('tahiw', 'ww');
 describe('tests for todoController', () => {
@@ -18,7 +18,7 @@ describe('tests to getAllTodos', () => {
 });
 describe('tests to EditTodo', () => {
   test('should add todo for a project', () => {
-    kProject.editToDo(0,'2wwww', 'zzz', 'Sunday', 'April 11th, 2021, 22:50', null, true);
+    kProject.editToDo(0, '2wwww', 'zzz', 'Sunday', 'April 11th, 2021, 22:50', null, true);
     const todos = ToDo('2wwww', 'zzz', 'Sunday', 'April 11th, 2021, 22:50', null, true);
     expect(todos.length).not.toBeNull();
   });
