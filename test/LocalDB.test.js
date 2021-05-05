73cfb82ba47463eb551127da101a995541601139
taiwo2 <adetonat@ymail.com>
2021-05-05 15:42:46 +0100

    fix linters errors

--- test/LocalDB.test.js
+++ test/LocalDB.test.js
@@ -2,14 +2,14 @@ import LocalDB from '../src/LocalDB';
 
 describe('tests to SaveProject to LocalDB', () => {
   test('should add todo for a project', () => {
-    const saveProject = LocalDB.saveProjects() ;
+    const saveProject = LocalDB.saveProjects();
     expect(saveProject).toBe(saveProject);
   });
 });
 
 describe('tests to RemoveTodo from LocalDB', () => {
   test('should add todo for a project', () => {
-    const removeDB = LocalDB.removeToDos('tawih') ;
+    const removeDB = LocalDB.removeToDos('tawih');
     expect(removeDB).toBe(removeDB);
   });
 });
--- test/Project.test.js
+++ test/Project.test.js
@@ -1,11 +1,11 @@
 import { Project, getAllToDos } from '../src/Project';
 import ToDo from '../src/Project';
 
-const kProject = Project('tahiw', 'ww') 
+const kProject = Project('tahiw', 'ww')
 describe('tests for todoController', () => {
   test('should add todo for a project', () => {
     kProject.addToDo('2wwww', 'zzz','Sunday', 'April 11th, 2021, 22:50', null, true);
-    const todos =ToDo('2wwww', 'zzz','Sunday', 'April 11th, 2021, 22:50', null, true);
+    const todos = ToDo('2wwww', 'zzz','Sunday', 'April 11th, 2021, 22:50', null, true);
     expect(todos.length).not.toBeNull();
   });
 });
@@ -13,14 +13,14 @@ describe('tests for todoController', () => {
 describe('tests to getAllTodos', () => {
   test('should add todo for a project', () => {
     kProject.getAllToDos('2wwww', 'zzz','Sunday', 'April 11th, 2021, 22:50', null, true);
-    const todos =ToDo('2wwww', 'zzz','Sunday', 'April 11th, 2021, 22:50', null, true);
+    const todos = ToDo('2wwww', 'zzz','Sunday', 'April 11th, 2021, 22:50', null, true);
     expect(todos.length).not.toBeNull();
   });
 });
 describe('tests to EditTodo', () => {
   test('should add todo for a project', () => {
     kProject.editToDo(0,'2wwww', 'zzz','Sunday', 'April 11th, 2021, 22:50', null, true);
-    const todos =ToDo('2wwww', 'zzz','Sunday', 'April 11th, 2021, 22:50', null, true);
+    const todos = ToDo('2wwww', 'zzz','Sunday', 'April 11th, 2021, 22:50', null, true);
     expect(todos.length).not.toBeNull();
   });
 });
